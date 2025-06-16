1a5d7cd14870aeac8f6b9abe756aea371dd817bd
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-06-16 21:30:09 +0200

    in the start of the next section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const cvButton = document.querySelector(".btn-color-2")
 cvButton.addEventListener("click", () => {
     window.open('/assets/other/cv.pdf')
 })
-const linkedIn = document.querySelector(".icon")
+const linkedIn = document.querySelector(".icon1")
 linkedIn.addEventListener("click", () => {
     window.open("https://www.linkedin.com/in/rasmus-carlsen-7592532b1")
 })
@@ -15,3 +15,13 @@ const contactMe = document.querySelector(".btn-color-1")
 contactMe.addEventListener('click', () => {
     location.href="#contact"
 })
+
+const githubLink = document.querySelector("#github-links1");
+githubLink.addEventListener("click", () => {
+    window.open("https://github.com/SpaceMasterCarlsen");
+});
+
+const websiteLink = document.querySelector("#website-links1");
+websiteLink.addEventListener("click", () => {
+    window.open("https://dmdatascience.dk/");
+});
